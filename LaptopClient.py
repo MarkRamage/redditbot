0b007a2df616b4729e81b9636ecb45b5d885e5f3
Mark A. Ramage <mark.a.ramage@vanderbilt.edu>
2017-04-13 16:59:39 -0500

    work in progress update

--- LaptopClient.py
+++ LaptopClient.py
@@ -1,19 +1,33 @@
 import zmq
+import string
 
 context = zmq.Context()
 
 # socket talks to server
+def run():
+    print("Connecting to hello world Server")
+    socket = context.socket(zmq.REQ)
+    socket.connect("tcp://localhost:5555")
+    running = True
+    question = input("Please enter your request for the server: ")
+    print("Sending request")
+    socket.send_string(question)
+    while running:
+        # get reply
+        print("getting reply")
+        message =socket.recv_string()
+        if message[0] == "0":
+            reply = input(message[1:len(message)]+": ")
+            socket.send_string(reply)
+        if message[0] == "1":
+            message = message[1:len(message)]
+            print(message.replace(u"\u2018", "'").replace(u"\u2019", "'"))
+            question = input("Please enter your request for the server: ")
+            print("Sending request")
+            socket.send_string(question)
+    
 
-print("Connecting to hello world Server")
-socket = context.socket(zmq.REQ)
-socket.connect("tcp://10.68.0.1:5555")
-
-
-
-print("Sending request")
-socket.send_string("Hello Server")
-# get reply
-message ="Received comments:" + socket.recv_string()
-print(message.encode('utf-8'))
-
+if  __name__ == "__main__":
+    run()
+    
 
--- raspverryServer.py
+++ raspverryServer.py
@@ -12,16 +12,62 @@ reddit = praw.Reddit(client_id='h_P0QRHoG1dhZQ',
                      username='MarkRamageVanderbilt')
 print(reddit.user.me())
 
-while True:
-    #wait for next request from client
-    message = socket.recv_string()
-    print("Recieved request: %s" % message)
-    comments = ""
-    for comment in reddit.inbox.messages():
-        comments += (comment.body) + "\n"
+def run():
+    while True:
+        #wait for next request from client
+        message = socket.recv_string()
+        reply = ""
+        if message == "inbox":
+            size = 0
+            for comment in reddit.inbox.all():
+                size=size+1
+                
+            reply+="0You have " + str(size) + " unread messages. Would you like to read them?"
+            answer = askClient(msg = reply)
+            while answer not in [1,0]:
+                answer = askClient(msg = "0Please answer yes/no")
+            if answer == 1:
+                reply = "1"
+                for comment in reddit.inbox.all():
+                    reply += (comment.body) + "\n"
+                if size == 0:
+                    reply += "No messages"
+            if answer == 0:
+                reply = "1Thank you"
+            if answer == -1:
+                reply = "1I didn't understand that"
+        elif message == "topics":
+            reply += "1Here are the hottest topcs for each of your favorite subbreddits"
+            for sub in ['games', 'all', 'askreddit']:
+                subreddit = reddit.subreddit(sub)
+                reply += "\n\n/r/"+sub
+                for topic in subreddit.new(limit = 3):
+                    reply +="\n    " + topic.title
+                    
+        else:
+        
+            reply = "1Please enter any of the following:\ninbox\ntopics"
         
 
-    #send replay back to client
-    socket.send_string("Hello Client, these are your messages" + comments)
+        #send replay back to client
+        
+        socket.send_string(reply)
+    
 
+    
+def askClient(msg = "you should not get this"):
+    global socket
+    socket.send_string(msg)
+    #get reply
+    reply = socket.recv_string()
+    print(reply)
+    reply = reply.lower()
+    if reply in ['1', 'yes', 'y']:
+        return 1
+    elif reply in ['0', 'no', 'n']:
+        return 0
+    else:
+        return -1
 
+if  __name__ == "__main__":
+    run()
