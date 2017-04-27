01569c613f63895d82df7ec8f8de74536af918cd
Mark A. Ramage <mark.a.ramage@vanderbilt.edu>
2017-04-27 13:14:52 -0500

    New abilities

--- LaptopClient.py
+++ LaptopClient.py
@@ -1,13 +1,18 @@
 import zmq
 import string
 
+#global variables
 context = zmq.Context()
+socket = context.socket(zmq.REQ)
 
+
+    
 # socket talks to server
 def run():
     print("Connecting to hello world Server")
-    socket = context.socket(zmq.REQ)
-    socket.connect("tcp://10.68.0.185:5555")
+    #socket.connect("tcp://10.68.0.185:5555")
+    socket.connect("tcp://localhost:5555")
+    socket.RCVTIMEO = 5000 
     running = True
     question = input("Please enter your request for the server: ")
     print("Sending request")
@@ -15,19 +20,64 @@ def run():
     while running:
         # get reply
         print("getting reply")
-        message =socket.recv_string()
+        try:
+            message =socket.recv_string()   #Ever loop starts with this, but if it timesout, we restart the client connection
+        except zmq.ZMQError as e:
+            print(e)
+            print("Server Failed to respond, resetting connection")
+            socket_restart() #this function restarts the program
+            question = input("Please enter your request for the server: ") #we always need to sends a request before ending the loop, even here.
+            print("Sending request")
+            socket.send_string(question)
+            continue    #restart while loop
+            
+        #A first character of zero implies that the server has asked a question, and wants a response
         if message[0] == "0":
             reply = input(message[1:len(message)]+": ")
+            print("Sending reply: "+reply)
             socket.send_string(reply)
+            
+        #A first character of 1 says that this is the final message in a text tree
         if message[0] == "1":
             message = message[1:len(message)]
             print(message.replace(u"\u2018", "'").replace(u"\u2019", "'"))
             question = input("Please enter your request for the server: ")
             print("Sending request")
             socket.send_string(question)
-    
+            
+        #When the first character is 2, then the server expects a full reddit comment
+        if message[0] == "2":
+            message = message[1:len(message)]
+            print(message.replace(u"\u2018", "'").replace(u"\u2019", "'"))
+            question =getRedditComment()
+            print("Sending request")
+            socket.send_string(question)
+            
+        #This simply closes the program after sending exit, quit, or q
+        if message[0] == "#":
+            exit()
+'''This function gathers and returns a multiline reddit comment. Reddit comments ignore 
+the third empty line, so three empty lines have been chosen as the cutoff point'''
+def getRedditComment():
+    emptyLines = 0
+    reply = ""
+    while emptyLines<3:
+        string = input()
+        if len(string) == 0:
+            emptyLines = emptyLines+1
+        else: #reset empty lines counter
+            emptyLines = 0
+        reply += string +"\n"
+    return reply            
+
+#resets the socket as needed
+def socket_restart():
+    global socket
+    socket.close()
+    socket = context.socket(zmq.REQ)
+    #socket.connect("tcp://10.68.0.185:5555")
+    socket.connect("tcp://localhost:5555")
+    socket.RCVTIMEO = 5000
 
 if  __name__ == "__main__":
     run()
-    
-
--- raspverryServer.py
+++ raspverryServer.py
@@ -1,6 +1,18 @@
 import time
 import zmq
 import praw
+import pickle
+
+subreddit_topics = {}
+delayedRepliesList = []
+
+
+try:
+    with open('topics_list.p', 'rb') as f:
+        subreddit_topics = dict(pickle.load(f))
+except IOError:
+    print("1: File does not exist")
+    pickle.dump(subreddit_topics, open('topics_list.p', 'wb'))
 
 try:
     file = open("login.txt", "r")
@@ -10,6 +22,7 @@ except IOError:
 context = zmq.Context()
 socket = context.socket(zmq.REP)
 socket.bind("tcp://*:5555")
+socket.RCVTIMEO = 5000 
 reddit = praw.Reddit(client_id=file.readline().rstrip(),
                      client_secret=file.readline().rstrip(),
                      password=file.readline().rstrip(),
@@ -18,13 +31,34 @@ reddit = praw.Reddit(client_id=file.readline().rstrip(),
 print(reddit.user.me())
 
 def run():
+    #main while loop of the function
     while True:
+        #check to see if we have a response to wait on
+        if len(delayedRepliesList) != 0:
+            print("checking")
+            for response in delayedRepliesList:
+                subreddit = reddit.subreddit(response[0])
+                for topic in subreddit.new(limit = 25):
+                    if response[1] in topic.title:
+                        try:
+                            topic.reply(response[2])
+                            delayedRepliesList.remove(response)
+                        except:
+                            print("Too soon")
+                        
+                        break
         #wait for next request from client
-        message = socket.recv_string()
+        try:
+            message =socket.recv_string()
+        except zmq.ZMQError as e:
+            continue
         reply = ""
-        if message == "inbox":
+        
+        
+        '''This will read out the unread messages in the users inbox'''
+        if message == "Inbox":
             size = 0
-            for comment in reddit.inbox.all():
+            for comment in reddit.inbox.unread():
                 size=size+1
                 
             reply+="0You have " + str(size) + " unread messages. Would you like to read them?"
@@ -33,7 +67,7 @@ def run():
                 answer = askClient(msg = "0Please answer yes/no")
             if answer == 1:
                 reply = "1"
-                for comment in reddit.inbox.all():
+                for comment in reddit.inbox.unread():
                     reply += (comment.body) + "\n"
                 if size == 0:
                     reply += "No messages"
@@ -41,39 +75,143 @@ def run():
                 reply = "1Thank you"
             if answer == -1:
                 reply = "1I didn't understand that"
-        elif message == "topics":
-            reply += "1Here are the hottest topcs for each of your favorite subbreddits"
-            for sub in ['games', 'all', 'askreddit']:
+        elif message == "Topics":
+            reply += "1Here are all of the watched topics in your favorite subreddits"
+            for sub in subreddit_topics:
                 subreddit = reddit.subreddit(sub)
                 reply += "\n\n/r/"+sub
-                for topic in subreddit.new(limit = 3):
-                    newTopic = "".join([i for i in topic.title if i.isprintable()])
-                    reply +="\n    " + newTopic
-                    
+                for topic in subreddit.new(limit = 25):
+                    if any(keyWord in topic.title.lower() for keyWord in subreddit_topics[sub]):
+                        newTopic = "".join([i for i in topic.title if i.isprintable()])
+                        reply +="\n    " + newTopic
+        elif message == "Subreddit":
+            reply+="0Here are all of the subreddits you have favorited"
+            for sub in subreddit_topics:
+                reply+="\n     "+sub
+            reply+="\nWould you like to add a new one?"
+            answer = askClient(msg = reply)
+            while answer not in [1,0]:
+                answer = askClient(msg = "0Please answer yes/no")
+            if answer == 1:
+                newSub = getreplyString(msg = "0What is this subreddit's name?")
+                if newSub not in subreddit_topics:
+                    subreddit_topics[newSub] = []
+                reply = "1Added new subreddit"
+                pickle.dump(subreddit_topics, open('topics_list.p', 'wb'))
+            if answer == 0:
+                reply = "1Thank you"
+            if answer == -1:
+                reply = "1I didn't understand that"
+        elif message == "RemoveSubreddit":
+            reply+="0Here are all of the subreddits you have favorited"
+            for sub in subreddit_topics:
+                reply+="\n     "+sub
+            reply+="\nWould you like to remove one?"
+            answer = askClient(msg = reply)
+            while answer not in [1,0]:
+                answer = askClient(msg = "0Please answer yes/no")
+            if answer == 1:
+                newSub = getreplyString(msg = "0What is this subreddit's name?")
+                if newSub in subreddit_topics:
+                    del subreddit_topics[newSub]
+                reply = "1Added new subreddit"
+                pickle.dump(subreddit_topics, open('topics_list.p', 'wb'))
+            if answer == 0:
+                reply = "1Thank you"
+            if answer == -1:
+                reply = "1I didn't understand that"
+        elif message == "SubTopic":
+            reply+="0Here are all of the subreddits you have favorited"
+            for sub in subreddit_topics:
+                reply+="\n     "+sub
+            reply+="\nWould you like to add a new favored topic to one?"
+            answer = askClient(msg = reply)
+            while answer not in [1,0]:
+                answer = askClient(msg = "0Please answer yes/no")
+            if answer == 1:
+                sub = getreplyString(msg = "0Which Subreddit are you changing?")
+                while sub not in subreddit_topics:
+                    sub = getreplyString(msg = "0That is not a recognized subreddit, please pick from the list, or add this subreddit")
+                reply="0Here is a list of topics being watched in this sub"
+                for topic in subreddit_topics[sub]:
+                    reply+="\n     "+topic
+                reply+="\nWhat new topic should I watch for?"
+                print(reply)
+                newTopic = getreplyString(msg = reply)
+                subreddit_topics[sub].append(newTopic)
+                reply = "1Topic Added"
+            if answer == 0:
+                reply = "1Thank you"
+            if answer == -1:
+                reply = "1I didn't understand that"
+        elif message == "RemoveSubTopic":
+            reply+="0Here are all of the subreddits you have favorited"
+            for sub in subreddit_topics:
+                reply+="\n     "+sub
+            reply+="\nWould you like to remove a favored topic in one?"
+            answer = askClient(msg = reply)
+            while answer not in [1,0]:
+                answer = askClient(msg = "0Please answer yes/no")
+            if answer == 1:
+                sub = getreplyString(msg = "0Which Subreddit are you changing?")
+                while sub not in subreddit_topics:
+                    sub = getreplyString(msg = "0That is not a recognized subreddit, please pick from the list, or add this subreddit")
+                reply="0Here is a list of topics being watched in this sub"
+                for topic in subreddit_topics[sub]:
+                    reply+="\n     "+topic
+                reply+="\nWhat  topic should I remove?"
+                print(reply)
+                delTopic = getreplyString(msg = reply)
+                if delTopic in subreddit_topics[sub]:
+                    subreddit_topics[sub].remove(delTopic)
+                
+                reply = "1Topic deleted"
+            if answer == 0:
+                reply = "1Thank you"
+            if answer == -1:
+                reply = "1I didn't understand that"
+        elif message == "SetResponse":
+            sub = getreplyString(msg = "0Which Subreddit are you setting a response to?") #get subreddit name
+            title = getreplyString(msg = "0What is the title, or partial title of this topic you wish to respond too?").lower() #get topic title, does not need to be full title
+            response = getreplyString(msg = "2Write your response, use enter as needed. Three blank lines ends the response.") #gets a full reddit comment
+            delayedRepliesList.append([sub, title, response]) #adds to list of delayed comments
+            reply = "1All set, it will be posted"
+        elif message.lower() in ['quit', 'exit', 'q']:
+            reply = "#Goodnight"
         else:
         
-            reply = "1Please enter any of the following:\ninbox\ntopics"
-        
-
+            reply = "1Please enter any of the following:\nInbox\nTopics\nSubreddit\nRemoveSubreddit\nSubTopic\nRemovenSubTopic"
         #send replay back to client
-        
         socket.send_string(reply)
-    
 
-    
 def askClient(msg = "you should not get this"):
     global socket
+    socket.RCVTIMEO = -1 #disable timeout while waiting
     socket.send_string(msg)
     #get reply
     reply = socket.recv_string()
     print(reply)
     reply = reply.lower()
+    while reply not in ['1', 'yes', 'y', '0', 'no', 'n']:
+        socket.send_string("0Please Enter Yes Or No")
+        reply = socket.recv_string()
+        reply = reply.lower()
+    socket.RCVTIMEO = 5000 
     if reply in ['1', 'yes', 'y']:
         return 1
     elif reply in ['0', 'no', 'n']:
         return 0
     else:
         return -1
-
+def getreplyString(msg = "you should not get this"):
+    global socket
+    socket.RCVTIMEO = -1 #disable timeout while waiting
+    socket.send_string(msg)
+    #get reply
+    reply = socket.recv_string()
+    print(reply)
+    reply = reply.lower()
+    socket.RCVTIMEO = 5000 
+    return reply
 if  __name__ == "__main__":
     run()
